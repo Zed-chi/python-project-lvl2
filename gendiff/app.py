6015a51f20e0b1d9a3865eaad44f4c709c944989
zed <zed@mail.ru>
2020-02-10 22:49:02 +0300

    black fix

--- gendiff/app.py
+++ gendiff/app.py
@@ -1,12 +1,17 @@
 import argparse
-#from differ import diff
-#from sourcer import source
+
+# from differ import diff
+# from sourcer import source
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Difference Generator app', add_help=True)
-    parser.add_argument('first_file', type=str, help='path for first file')
-    parser.add_argument('second_file', type=str, help='path for second file')
-    parser.add_argument('-f', "--format", type=bool, help="set format of output")
+    parser = argparse.ArgumentParser(
+        description="Difference Generator app", add_help=True
+    )
+    parser.add_argument("first_file", type=str, help="path for first file")
+    parser.add_argument("second_file", type=str, help="path for second file")
+    parser.add_argument(
+        "-f", "--format", type=bool, help="set format of output"
+    )
     args = parser.parse_args()
     print(args)
