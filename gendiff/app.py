7b32301805f16c8d96c87e96bc1a1764756a6d49
zed <zed@mail.ru>
2020-02-10 22:38:57 +0300

    upd

--- gendiff/app.py
+++ gendiff/app.py
@@ -1,10 +1,12 @@
 import argparse
+#from differ import diff
+#from sourcer import source
 
 
 def main():
     parser = argparse.ArgumentParser(description='Difference Generator app', add_help=True)
     parser.add_argument('first_file', type=str, help='path for first file')
     parser.add_argument('second_file', type=str, help='path for second file')
-    parser.add_argument('-f', "--format", type=bool,)
+    parser.add_argument('-f', "--format", type=bool, help="set format of output")
     args = parser.parse_args()
     print(args)
