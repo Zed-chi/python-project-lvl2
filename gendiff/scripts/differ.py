808218b17655a75a93f24e69e0add091a6d501e3
zed <zed@mail.ru>
2020-02-13 01:45:39 +0300

    added yml realization

--- gendiff/scripts/differ.py
+++ gendiff/scripts/differ.py
@@ -1,5 +1,6 @@
 from json import loads
 from .utils import get_file_content
+from yaml import load
 
 
 def get_diff_summary(filepath1, filepath2, format="json"):
@@ -12,6 +13,16 @@ def get_diff_summary(filepath1, filepath2, format="json"):
         diff_b = set(b.keys()).difference(set(a.keys()))
         common = set(a.keys()).intersection(set(b.keys()))
         return [a, b, diff_a, diff_b, common]
+    else:
+        a = load(get_file_content(filepath1))
+        b = load(get_file_content(filepath2))
+        print(a)
+        print(b)
+        diff_a = set(a.keys()).difference(set(b.keys()))
+        diff_b = set(b.keys()).difference(set(a.keys()))
+        common = set(a.keys()).intersection(set(b.keys()))
+        return [a, b, diff_a, diff_b, common]
+    
 
 
 def diff_to_str(a, b, diff_a, diff_b, common, format="json"):
